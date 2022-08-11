fbcc89f18c1846606377e8b2f6eb0d0c3febbfc2
maryamsoftdev <nabeelahmaryaam@gmail.com>
2022-08-11 23:28:08 +0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+// import { BrowserRouter, Route } from 'react-router-dom'
+// import ReactDOM from "react-dom/client";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Login from './pages/Login'
+import Register from './pages/Register'
+import Dashboard from './pages/Dashboard'
+const App = ()=>{
+    return (
+    <div>
+        <BrowserRouter>
+         <Routes>
+           {/* <Route exact path='/login' element={<Login/>} /> */}
+         <Route path='/login' element={<Login />}></Route>
+          <Route path='/register' element={<Register />}></Route>
+          <Route path='/dashboard' element={<Dashboard />}></Route>
+        {/* <Route exact path='/register' element={<Register/>} /> */}
+         </Routes>
+        </BrowserRouter>
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
-  );
+    )
 }
+export default App
 
-export default App;
+// root.render(
+//   <BrowserRouter>
+//     <Routes>
+//       <Route path="/" element={<App />} />
+//       <Route path="expenses" element={<Expenses />} />
+//       <Route path="invoices" element={<Invoices />} />
+//     </Routes>
+//   </BrowserRouter>
+// );
+
+
